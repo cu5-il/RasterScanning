e7f5328dacdb50f551d4aff61b6efad2ed6b15e0
Chris Urbanski <cu5@illinois.edu>
2021-11-28 15:43:53 -0600

    fixed external definitions and declarations

--- scan_and_process/myGlobals.cpp
+++ scan_and_process/myGlobals.cpp
@@ -2,3 +2,9 @@
 
 A3200Handle handle = NULL;
 A3200DataCollectConfigHandle DCCHandle = NULL;
+
+threadsafe_queue <cv::Mat> q_scannedEdges;
+bool doneScanning = false;
+bool positionFlag = false;
+
+int segmentNum = 0;
