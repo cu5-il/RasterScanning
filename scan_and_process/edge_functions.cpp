41dde9db1248c3df3041512ce238bd2c2633d11a
Chris Urbanski <cu5@illinois.edu>
2022-01-26 10:15:27 -0600

    Added function to calculate errors at specific waypoints. Moved function comments to header

--- scan_and_process/edge_functions.cpp
+++ scan_and_process/edge_functions.cpp
@@ -19,14 +19,6 @@ struct sortDist {
 	cv::Point pt0;
 };
 
-/**
- * @brief Breaks up the raster pattern into segments. Each vertical rod in the raster is a segment; corners are neglected.
- * @param[in] rasterCoords Vector of (x,y) points of the raster pattern
- * @param[in] ROIwidth of the ROI
- * @param[out] ROIs Vector of regions that are around each rod. Used to determine which edges belong to which segment
- * @param[out] centerlines Vector of point pairs defining the centerline of the region
- * @param[out] scanDonePts Point in the raster pattern when the region has been finished scanning. Set to midpoint of the following rod except for the last region, which is just the offset of the last point of the raster
-*/
 void makeSegments(const std::vector<cv::Point>& rasterCoords, double ROIwidth, std::vector<Segment>& seg, cv::Point2d initPos) {
 	std::vector<cv::Rect> ROIs;
 	std::vector<std::vector<cv::Point>> centerlines;
@@ -58,14 +50,6 @@ void makeSegments(const std::vector<cv::Point>& rasterCoords, double ROIwidth, s
 	// TODO: add corner regions
 }
 
-/**
- * @brief Finds the left and right edges of the material and smooths them
- * @param[in] segmentROI Rectangle specifying the region of the image to search for the edges 
- * @param[in] gblEdges Image of edge points found by the scanner
- * @param[out] lEdgePts Filtered points that make up the edge in the left half of the ROI
- * @param[out] rEdgePts Filtered points that make up the edge in the right half of the ROI
- * @param[in] interp Flag indicating whether to interpolate the output edge points 
-*/
 void getMatlEdges(const cv::Rect& segmentROI, const cv::Mat& gblEdges, std::vector<cv::Point>& lEdgePts, std::vector<cv::Point>& rEdgePts, bool interp = false) {
 	std::vector<cv::Point> unfiltLeft, unfiltRight;
 	cv::Mat interpPts;
@@ -106,18 +90,7 @@ void getMatlEdges(const cv::Rect& segmentROI, const cv::Mat& gblEdges, std::vect
 	}
 }
 
-/**
- * @brief Calculates the material centerline and width errors.
- * @param[in,out] centerline Vector of points that make up the desired centerline. Input as 2 coordinate pairs; output as interpolated coordinate pairs  
- * @param[in] width Desired width of the material in mm
- * @param[in] rasterSize Size of the raster image
- * @param[in] lEdgePts Points making up the left edge of the rod
- * @param[in] rEdgePts Points making up the right edge of the rod
- * @param[out] errCL Material centerline error in pixels
- * @param[out] errWD Material width error in pixels
-*/
 void getMatlErrors(std::vector<cv::Point>& centerline, double width, cv::Size rasterSize, const std::vector<cv::Point>& lEdgePts, const std::vector<cv::Point>& rEdgePts, std::vector<double>& errCL, std::vector<double>& errWD) {
-	// Calculate Errors
 	cv::Mat lEdge = cv::Mat(rasterSize, CV_8UC1, cv::Scalar(255)); // Image to draw the left edge on
 	cv::Mat rEdge = cv::Mat(rasterSize, CV_8UC1, cv::Scalar(255)); // Image to draw the right edge on
 	cv::LineIterator lnit(lEdge, centerline.front(), centerline.back(), 8);
@@ -140,5 +113,28 @@ void getMatlErrors(std::vector<cv::Point>& centerline, double width, cv::Size ra
 		centerline.push_back(lnit.pos()); // add the points from the centerline
 		errCL.push_back((rEdge.at<float>(lnit.pos()) - lEdge.at<float>(lnit.pos())) / 2);
 		errWD.push_back(lEdge.at<float>(lnit.pos()) + rEdge.at<float>(lnit.pos()) - MM2PIX(width));
+	}
+}
+
+void getErrorsAt(std::vector<cv::Point>& waypoints, double width, cv::Size rasterSize, const std::vector<cv::Point>& lEdgePts, const std::vector<cv::Point>& rEdgePts, std::vector<double>& errCL, std::vector<double>& errWD) {
+	cv::Mat lEdge = cv::Mat(rasterSize, CV_8UC1, cv::Scalar(255)); // Image to draw the left edge on
+	cv::Mat rEdge = cv::Mat(rasterSize, CV_8UC1, cv::Scalar(255)); // Image to draw the right edge on
+	errCL.clear(); // clear the errors
+	errWD.clear();
+	errCL.reserve(waypoints.size());
+	errWD.reserve(waypoints.size());
+	// See how far the edges are from the unmodified path
+	// Draw the smoothed edges on an image
+	cv::polylines(lEdge, lEdgePts, false, cv::Scalar(0), 1);
+	cv::polylines(rEdge, rEdgePts, false, cv::Scalar(0), 1);
+	// apply a distance transform to the image with the smoothed edges
+	cv::distanceTransform(lEdge, lEdge, cv::DIST_L2, cv::DIST_MASK_PRECISE, CV_32F); //NOTE: regarding speed -  DIST_MASK_5 (3.9ms/rod) < DIST_MASK_PRECISE (4.7ms/rod) < DIST_MASK_3 (9.1ms/rod) 
+	cv::distanceTransform(rEdge, rEdge, cv::DIST_L2, cv::DIST_MASK_PRECISE, CV_32F);
+	// iterate over the target centerline to calculate the errors
+	for (auto it = waypoints.begin(); it != waypoints.end(); ++it) {
+		//TODO: check if there is a measured edge to the left / right of the path
+		// maybe check if the dXform distance is greater than the boundary width
+		errCL.push_back((rEdge.at<float>(*it) - lEdge.at<float>(*it)) / 2);
+		errWD.push_back(lEdge.at<float>(*it) + rEdge.at<float>(*it) - MM2PIX(width));
 	}
 }
--- scan_and_process/edge_functions.h
+++ scan_and_process/edge_functions.h
@@ -5,10 +5,48 @@
 #ifndef EDGE_FNS_H
 #define EDGE_FNS_H
 
+/**
+ * @brief Breaks up the raster pattern into segments. Each vertical rod in the raster is a segment; corners are neglected.
+ * @param[in] rasterCoords Vector of (x,y) points of the raster pattern
+ * @param[in] ROIwidth of the ROI
+ * @param[out] ROIs Vector of regions that are around each rod. Used to determine which edges belong to which segment
+ * @param[out] centerlines Vector of point pairs defining the centerline of the region
+ * @param[out] scanDonePts Point in the raster pattern when the region has been finished scanning. Set to midpoint of the following rod except for the last region, which is just the offset of the last point of the raster
+*/
 void makeSegments(const std::vector<cv::Point>& rasterCoords, double ROIwidth, std::vector<Segment>& seg, cv::Point2d initPos);
 
+/**
+ * @brief Finds the left and right edges of the material and smooths them
+ * @param[in] segmentROI Rectangle specifying the region of the image to search for the edges
+ * @param[in] gblEdges Image of edge points found by the scanner
+ * @param[out] lEdgePts Filtered points that make up the edge in the left half of the ROI
+ * @param[out] rEdgePts Filtered points that make up the edge in the right half of the ROI
+ * @param[in] interp Flag indicating whether to interpolate the output edge points
+*/
 void getMatlEdges(const cv::Rect& segmentROI, const cv::Mat& gblEdges, std::vector<cv::Point>& lEdgePts, std::vector<cv::Point>& rEdgePts, bool interp = false);
 
+/**
+ * @brief Calculates the material centerline and width errors.
+ * @param[in,out] centerline Vector of points that make up the desired centerline. Input as 2 coordinate pairs; output as interpolated coordinate pairs
+ * @param[in] width Desired width of the material in mm
+ * @param[in] rasterSize Size of the raster image
+ * @param[in] lEdgePts Points making up the left edge of the rod
+ * @param[in] rEdgePts Points making up the right edge of the rod
+ * @param[out] errCL Material centerline error in pixels
+ * @param[out] errWD Material width error in pixels
+*/
 void getMatlErrors(std::vector<cv::Point>& centerline, double width, cv::Size rasterSize, const std::vector<cv::Point>& lEdgePts, const std::vector<cv::Point>& rEdgePts, std::vector<double>& errCL, std::vector<double>& errWD);
+
+/**
+ * @brief Calculates the material centerline and width errors at the input waypoints.
+ * @param[in] waypoints Vector of waypoints in pixel coordinates where the errors should be calculated
+ * @param[in] width Desired width of the material in mm
+ * @param[in] rasterSize Size of the raster image
+ * @param[in] lEdgePts Points making up the left edge of the rod
+ * @param[in] rEdgePts Points making up the right edge of the rod
+ * @param[out] errCL Material centerline error in pixels
+ * @param[out] errWD Material width error in pixels
+*/
+void getErrorsAt(std::vector<cv::Point>& waypoints, double width, cv::Size rasterSize, const std::vector<cv::Point>& lEdgePts, const std::vector<cv::Point>& rEdgePts, std::vector<double>& errCL, std::vector<double>& errWD);
 
 #endif // EDGE_FNS_H
