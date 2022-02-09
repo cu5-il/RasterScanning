a58a049041beb7ef1f115a706b906eca220605ab
Chris Urbanski <cu5@illinois.edu>
2022-02-09 13:53:47 -0600

    Fixed issue with order of operations

--- scan_and_process/constants.h
+++ scan_and_process/constants.h
@@ -16,7 +16,7 @@
 
 #define SCAN_WIDTH 26.4
 #define RESOLUTION 0.1/*0.02*/  // 1 pixel = RESOLUTION mm
-#define PIX2MM(pix)  pix*RESOLUTION
+#define PIX2MM(pix)  (pix)*RESOLUTION
 #define MM2PIX(mm)  std::lround( (mm) / RESOLUTION)
 #define RASTER_IMG_WIDTH 11.0 //TODO: incorporate this into the makeRaster Function
 #define PI 3.14159265
