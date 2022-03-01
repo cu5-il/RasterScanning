653079dd1dc1f5597a8d07837fd269c576fa55ec
Chris Urbanski <cu5@illinois.edu>
2022-03-01 12:35:23 -0600

    Removed unused defines

--- scan_and_process/constants.h
+++ scan_and_process/constants.h
@@ -12,26 +12,23 @@
 #define OPAMP_GAIN -0.49875
 #define TASK_SCAN TASKID_04
 #define TASK_PRINT TASKID_01
-#define TASK_EXTRUDE TASKID_02
 
 #define SCAN_WIDTH 26.4
 #define RESOLUTION 0.05/*0.02*/  // 1 pixel = RESOLUTION mm
 #define PIX2MM(pix)  (pix)*RESOLUTION
 #define MM2PIX(mm)  std::lround( (mm) / RESOLUTION)
-#define RASTER_IMG_WIDTH 11.0 //TODO: incorporate this into the makeRaster Function
 #define PI 3.14159265
 
 #define SCAN_OFFSET_X -13.0
 #define SCAN_OFFSET_Y 0.7
-#define VELOCITY 1
+
 
 #define CVPLOT_HEADER_ONLY
 
 #define AXES_ALL (AXISMASK)(AXISMASK_00 | AXISMASK_01 | AXISMASK_02 | AXISMASK_03)
 #define DISPOSAL_X 00
 #define DISPOSAL_Y 153
-//#define LEADIN_LINE 5
-//#define LEADOUT_LINE 5
+
 #define SAFE_Z 5
 
 //#define DEBUG_SCANNING
