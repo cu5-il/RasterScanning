f01e623808c362eb8acb6676736331517043ab3a
Chris Urbanski <cu5@illinois.edu>
2022-03-16 16:25:18 -0500

    Changed the resolution

--- Robert/include/constants.h
+++ Robert/include/constants.h
@@ -8,7 +8,7 @@
 #define TASK_PRINT TASKID_01
 #define AXES_ALL (AXISMASK)(AXISMASK_00 | AXISMASK_01 | AXISMASK_02 | AXISMASK_03)
 
-#define RESOLUTION 0.05/*0.02*/  // 1 pixel = RESOLUTION mm
+#define RESOLUTION 0.02/*0.05*/  // 1 pixel = RESOLUTION mm
 #define PIX2MM(pix)  (pix)*RESOLUTION
 #define MM2PIX(mm)  std::lround( (mm) / RESOLUTION)
 #define PI 3.14159265
