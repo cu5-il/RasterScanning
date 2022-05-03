da8d07a6d574554f5e6564ce535b49d583027f57
Chris Urbanski <cu5@illinois.edu>
2022-05-03 14:38:01 -0500

    Updated to use new table input and multi-layer scaffolds

--- ControlCalibration/main.cpp
+++ ControlCalibration/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#include <iomanip>      // std::setw
 #include <vector> 
 #include <string>
-#include <algorithm>
-#include <iterator> 
-#include <valarray>
+#include <numeric> // std::accumulate
+
 #include <deque>
-#include <Windows.h>
+#include <filesystem> // std::filesystem::copy, std::filesystem::create_directories
+namespace fs = std::filesystem;
 
 #include <opencv2/core.hpp>
 #include "opencv2/core/utility.hpp"
@@ -23,171 +23,257 @@
 #include "myGlobals.h"
 #include "scanning.h"
 #include "draw.h"
-#include "gaussianSmooth.h"
-#include "errors.h"
+
 #include "A3200_functions.h"
 #include "thread_functions.h"
-#include "csvMat.h"
 #include "raster.h"
 #include <ctime>
 #include "path.h"
-#include "print.h"
 #include "controlCalib.h"
+#include "input.h"
+#include "multiLayer.h"
 
 std::string datetime(std::string format = "%Y.%m.%d-%H.%M.%S");
 
 int main() {
 	// Disable openCV warning in console
 	cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
-	// Set the output path with and prepend all file names with the time
+	// Create and set the output path
 	outDir.append(datetime("%Y.%m.%d") + "/");
-	if (CreateDirectoryA(outDir.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError()) {}
-	else{
-		std::cout << "Error creating output directory" << std::endl;
-		system("pause");
-		return 0;
-	}
+	try { fs::create_directories(outDir); }
+	catch (std::exception& e) { std::cout << e.what(); }
+
+	//=======================================
+	// Connecting to and setting up the A3200
+	std::cout << "Connecting to A3200. Initializing if necessary." << std::endl;
+	if (!A3200Connect(&handle)) { A3200Error(); }
+	// Creating a data collection handle and setting up the data collection
+	if (!A3200DataCollectionConfigCreate(handle, &DCCHandle)) { A3200Error(); }
+	if (!setupDataCollection(handle, DCCHandle)) { A3200Error(); }
+	// Initializing the extruder
+	extruder = Extruder(handle, TASK_PRINT);
+	//=======================================
 
 	std::thread t_scan, t_process, t_control, t_print;
 
 	// Initialize parameters
-	double initVel;
-	double initExt;
-	cv::Point3d initPos;
-	double wayptSpc = 1;
 	Raster raster;
-	std::string testTp;
-	double range[2];
-	std::vector<std::vector<Path>> path;
+	double rasterBorder = 2;
+	std::vector<std::vector<Path>> path, ctrlPath;
+
+	// defining the material models
+	MaterialModel augerModel = MaterialModel('a',
+		std::vector<double>{2, 3},
+		std::vector<double>{1, 1},
+		std::vector<double>{1, 1},
+		std::vector<double>{0, 0});
+
+	// setting up the controller
+	AugerController controller(augerModel, 0.1, 1.5);
 
 	// setting the print options
-	double leadin = 5;
-	double leadout = 5;
-	PrintOptions printOpts(leadin, -1, true);
+	double leadin = 10;
+	PrintOptions printOpts(leadin);
+	printOpts.extrude = true;
+	printOpts.disposal = false;
+	printOpts.asyncTheta = 32;
 
 	// Getting user input
-	std::string resp, file;
+	std::string resp, infile;
+	char option;
 	int lineNum;
-	std::cout << "Select option: (p)rint, (s)can, or (a)nalyze data? " ;
-	std::cin >> resp;
-	if (resp.compare("a") == 0) {
-		std::cout << "Enter test name: ";
-		//std::cin >> file;
-		file = "plate3";
-		lineNum = 1;
-		// read in test parameters and generate raster
-		if (!readTestParams(std::string("./Input/" + file + ".txt"), raster, wayptSpc, initPos, initVel, initExt, testTp, range, lineNum)) { return 0; }
-		makePath(raster, wayptSpc, 0, initPos, initVel, 0, segments, path);
+	infile = "./Input/printTable.md";
 
-		file = "./Output/2022.02.18/auger2/a10_17.44";
-		cv::Mat edges = cv::Mat::zeros(raster.size(), CV_8U);
-		readCSV(file + "_edges.csv", edges);
-		edges.convertTo(edges, CV_8U);
-		cv::imwrite(file + "_edgedata.png", edges);
-		//edges = cv::imread(file + "_edgedata.png");
-		//edges.convertTo(edges, CV_8U);
-
-		// Analyzing the print
-		outDir = file + "_";
-		//analyzePrint(raster, std::string(file + "_edgedata.png"));
-		return 0;
-	}
-	else if (resp.compare("p") != 0 && resp.compare("s") != 0 ) {
-		return 0;
-	}
+	std::cout << "Select option: (p)rint or (s)can? ";
+	std::cin >> option;
 	std::cout << "Test #: ";
 	std::cin >> lineNum;
-	file = "plate2";
-	// read in test parameters and generate raster
-	if (!readTestParams(std::string("./Input/" + file + ".txt"), raster, wayptSpc, initPos, initVel, initExt, testTp, range, lineNum)) { return 0; }
-	outDir.append(testTp + std::to_string(lineNum) + "_"+ datetime("%H.%M") + "_");
 
+	TableInput input(infile, lineNum);
+	// copy the table to the output folder
+	try { fs::copy(infile, outDir, fs::copy_options::overwrite_existing); }
+	catch (std::exception& e) { std::cout << e.what(); }
 
-	// Creating the path and segmets
-	if (resp.compare("p") == 0) {
-		makePath(raster, wayptSpc, 0, initPos, initVel, initExt, segments, path);
-		// Modifying the inputs
-		makeCalibPath(path, testTp[0], range);
-	}
-	else if (resp.compare("s") == 0) {
-		initVel = 2;
-		//Raster rasterScan = Raster(raster.length() - SCAN_OFFSET_X + raster.rodWidth(), raster.width(), raster.spacing(), raster.rodWidth());
-		Raster rasterScan = Raster(raster.length() + 2 * raster.rodWidth(), raster.width(), raster.spacing(), raster.rodWidth());
-		rasterScan.offset(cv::Point2d(initPos.x, initPos.y));
-		rasterScan.offset(cv::Point2d(-SCAN_OFFSET_X - raster.rodWidth()));
-		//initPos += cv::Point3d(0, 0, 2);
-		makePath(rasterScan, wayptSpc, 0, initPos, initVel, 0, segments, path);
-	}
-	int segsBeforeCtrl = path.size();
-	
-	cv::Mat imSeg;
-	drawSegments(raster.draw(), imSeg, segments, raster.origin(), 3);
+	// Append the output directory with the print number and create the directory
+	outDir.append("/print" + std::to_string(lineNum) + "/");
+	try { fs::create_directories(outDir); }
+	catch (std::exception& e) { std::cout << e.what(); }
 
-	//goto cleanup;
+	// make the scaffold
+	raster = Raster(input.length, input.width, input.rodSpc, input.rodSpc - .1, rasterBorder);
+	raster.offset(cv::Point2d(input.initPos.x, input.initPos.y));
+	//MultiLayerScaffold scaffold(input, raster);
+	FunGenScaf scaffold(input, raster, augerModel);
+	// add a lead out line
+	printOpts.leadout = -SCAN_OFFSET_X + 1;
+	scaffold.leadout(-SCAN_OFFSET_X);
 
-	// Sanity check
-	//system("pause");
+	path = scaffold.path;
+	segments = scaffold.segments;
 
-	// A3200 Setup
-	//=======================================
-	//Connecting to the A3200
+#ifdef DEBUG_SCANNING
+	q_scanMsg.push(true);
+	t_CollectScans(raster);
+	return 0;
+#endif // DEBUG_SCANNING
+
+	cv::Mat imseg = raster.draw(input.startLayer);
+	drawSegments(raster.draw(input.startLayer), imseg, segments, raster.origin(), input.startLayer, 3);
+	cv::Mat image = cv::Mat::zeros(raster.size(segments.back().layer()), CV_8UC3);
+	drawMaterial(image, image, scaffold.segments, scaffold.path, scaffold.segments.back().layer());
+
+	switch (option)
 	{
-		std::cout << "Connecting to A3200. Initializing if necessary." << std::endl;
-		if (!A3200Connect(&handle)) { A3200Error(); }
-		// Creating a data collection handle and setting up the data collection
-		if (!A3200DataCollectionConfigCreate(handle, &DCCHandle)) { A3200Error(); }
-		if (!setupDataCollection(handle, DCCHandle)) { A3200Error(); }
-		// Disabling the auger and air
-		if (!A3200IODigitalOutput(handle, TASKID_Library, 0, AXISINDEX_00, 0)) { A3200Error(); } //equivalent to $WO[0].X = 0
-		// Homing the axes if not already done
-		std::cout << "Homing axes." << std::endl;
-		if (!A3200MotionEnable(handle, TASKID_Library, AXES_ALL)) { A3200Error(); }
-		if (!A3200MotionHomeConditional(handle, TASKID_Library, (AXISMASK)(AXISMASK_03))) { A3200Error(); } // TH axis 
-		if (!A3200MotionHomeConditional(handle, TASKID_Library, (AXISMASK)(AXISMASK_02))) { A3200Error(); } // Z axis 
-		if (!A3200MotionHomeConditional(handle, TASKID_Library, (AXISMASK)(AXISMASK_00 | AXISMASK_01))) { A3200Error(); } // X & Y axes 
-		if (!A3200MotionWaitForMotionDone(handle, AXES_ALL, WAITOPTION_InPosition, -1, NULL)) { A3200Error(); }
-		if (!A3200MotionDisable(handle, TASKID_Library, AXES_ALL)) { A3200Error(); }
-		// End any program already running
-		if (!A3200ProgramStop(handle, TASK_PRINT)) { A3200Error(); }
-		// Initializing the extruder
-		extruder = Extruder(handle, TASK_PRINT);
-		// Clear the messages and the indicators in the CNC interface
-		if (!A3200CommandExecute(handle, TASK_PRINT, (LPCSTR)"MSGCLEAR -1\n", NULL)) { A3200Error(); }
-		for (int i = 1; i <= 6; i++) {
-			if (!A3200CommandExecute(handle, TASK_PRINT, std::string("MSGLAMP " + std::to_string(i) + ", GRAY, \"\"\n").c_str(), NULL)) { A3200Error(); }
+	default:
+		return 0;
+	case 'p': // PRINTING
+		outDir.append("print_");
+		printOpts.asyncTheta = 0;
+		ctrlPath = path;
+
+		//t_scan = std::thread{ t_CollectScans, raster };
+		//t_process = std::thread{ t_GetMatlErrors, raster, path };
+		t_print = std::thread{ t_printQueue, path[0][0], printOpts };
+		t_control = std::thread{ t_noController, ctrlPath };
+
+		//t_scan.join();
+		//t_process.join();
+		t_control.join();
+
+		break;
+
+	case 's': // SCANNING
+		Raster rasterScan;
+		segments.clear();
+		path.clear();
+		printOpts.extrude = false;
+		//std::cout << "(r)otating or (f)ixed scan: ";
+		//std::cin >> option;
+		option = 'f';
+		switch (option)
+		{
+		default:
+			return 0;
+		case 'r':
+			outDir.append("scanR_");
+			rasterScan = raster;
+			if (input.startLayer % 2 == 0) { rasterScan.offset(cv::Point2d(0, 1.5)); } // offset path in y direction
+			else { rasterScan.offset(cv::Point2d(1.5, 0)); } // offset path in x direction
+			input.initPos += cv::Point3d(0, 0, 1); // raise path
+			scaffold = FunGenScaf(input, rasterScan, augerModel);
+			scaffold.leadout(-SCAN_OFFSET_X);
+			segments = scaffold.segments;
+			path = scaffold.path;
+			ctrlPath = scaffold.path;
+
+			t_scan = std::thread{ t_CollectScans, raster };
+			t_process = std::thread{ t_GetMatlErrors, raster, path };
+			t_print = std::thread{ t_printQueue, path[0][0], printOpts };
+			t_control = std::thread{ t_controller, std::ref(ctrlPath), std::ref(controller) };
+
+			t_scan.join();
+			t_process.join();
+			t_control.join();
+			break;
+		case 'f':
+			outDir.append("scanF_");
+			printOpts.leadin = 0;
+			printOpts.leadout = 0;
+			printOpts.asyncTheta = 0;
+
+			segments = scaffold.segmentsScan;
+			path = scaffold.pathScan;
+			ctrlPath = scaffold.pathScan;
+			t_scan = std::thread{ t_CollectScans, raster };
+			t_print = std::thread{ t_printQueue, path[0][0], printOpts };
+			t_control = std::thread{ t_noController, ctrlPath };
+
+			t_scan.join();
+			t_control.join();
+
+			// Calculate the errors using the actual raster path used in the print
+			segments.clear();
+			path.clear();
+			path = scaffold.path;
+			ctrlPath = scaffold.path;
+			segments = scaffold.segments;
+
+			t_GetMatlErrors(raster, path);
+			break;
+		}
+
+		break;
+	}
+
+	// Opening a file to save the results
+	std::ofstream outfile;
+	outfile.open(std::string(outDir + "pathData.txt").c_str());
+	outfile.precision(3);
+	// loop through each long segment
+	for (int i = 0; i < path.size(); i += 2) {
+		// loop through all the waypoints
+		for (int j = 0; j < path[i].size(); j++) {
+			outfile << std::setw(3) << std::fixed << i << "\t";
+			outfile << std::setw(7) << std::fixed << ctrlPath[i][j].x << "\t";
+			outfile << std::setw(7) << std::fixed << ctrlPath[i][j].y << "\t";
+			outfile << std::setw(6) << std::fixed << ctrlPath[i][j].f << "\t";
+			outfile << std::setw(6) << std::fixed << ctrlPath[i][j].e << "\t";
+			outfile << std::setw(6) << std::fixed << ctrlPath[i][j].w << "\t";
+			if (!segments[i].errWD().empty())
+				outfile << std::setw(9) << std::fixed << segments[i].errWD()[j] << "\t";
+			if (!segments[i].errCL().empty())
+				outfile << std::setw(9) << std::fixed << segments[i].errCL()[j];
+			outfile << "\n";
 		}
 	}
-	//=======================================
-	
-	//// notify scanner to start
-	//q_scanMsg.push(true);
-	//t_CollectScans(raster);
+	outfile.close();
 
-	// just printing, no scanning
-	if (resp.compare("p") == 0){
-		printOpts.extrude = true;
-		t_control = std::thread{ t_noController, path };
-		t_print = std::thread{ t_printQueue, path[0][0], printOpts };
-		t_print.join();
-		t_control.join();
-		goto cleanup;
+	if (option == 'f' || option == 'r')
+	{
+		// calculate the error norms
+		double E2d;
+		auto sumsq = [](double a, double b) {
+			if (!std::isnan(b)) return a + b * b;
+			else return a; };
+		outfile.open(std::string(outDir + "errors.txt").c_str());
+		outfile.precision(3);
+		// loop through each long segment
+		for (int i = 0; i < path.size(); i += 2) {
+			E2d = sqrt(std::accumulate(segments[i].errWD().begin(), segments[i].errWD().end(), 0.0, sumsq));
+			E2d /= input.wayptSpc * (std::count_if(segments[i].errWD().begin(), segments[i].errWD().end(), [](double a) {return !std::isnan(a); }) - 1);
+			outfile << std::setw(6) << std::fixed << E2d;
+			if (i % 4 == 0) outfile << "\t";
+			else outfile << "\n";
+		}
+		outfile.close();
+
+		// calculate the average width of each segment
+		double mean, stdev, meanErr;
+		auto sum = [](double a, double b) {
+			if (!std::isnan(b)) return a + b;
+			else return a; };
+		auto variance = [&meanErr](double a, double b) {
+			if (!std::isnan(b)) return a + (b - meanErr) * (b - meanErr);
+			else return a; };
+		outfile.open(std::string(outDir + "widths.txt").c_str());
+		outfile.precision(3);
+		// loop through each long segment
+		for (int i = 0; i < path.size(); i += 2) {
+			// calculate the mean
+			meanErr = std::accumulate(segments[i].errWD().begin(), segments[i].errWD().end(), 0.0, sum);
+			meanErr /= (std::count_if(segments[i].errWD().begin(), segments[i].errWD().end(), [](double a) {return !std::isnan(a); }) - 1);
+			mean = path[i][0].w - meanErr;
+			// calculate the standard deviation
+			stdev = std::accumulate(segments[i].errWD().begin(), segments[i].errWD().end(), 0.0, variance);
+			stdev /= (std::count_if(segments[i].errWD().begin(), segments[i].errWD().end(), [](double a) {return !std::isnan(a); }) - 1);
+			stdev = sqrt(stdev);
+			outfile << std::setw(6) << std::fixed << path[i][0].f << "\t" << path[i][0].e << "\t" << mean << "\t" << stdev << "\n";
+		}
+		outfile.close();
 	}
-	// Scanning, no error calculations
-	else if (resp.compare("s") == 0) {
-		printOpts.extrude = false;
-		t_scan = std::thread{ t_CollectScans, raster };
-		t_control = std::thread{ t_noController, path };
-		t_print = std::thread{ t_printQueue, path[0][0], printOpts };
-		t_print.join();
-		t_scan.join();
-		t_control.join();
-		// calculate the average width of the segments
-		segments.clear();
-		makePath(raster, wayptSpc, 0, initPos, initVel, initExt, segments, path);
-		t_GetMatlErrors(raster, path);
-		analyzePrint(raster);
-		goto cleanup;
-	}
+
+	t_print.join();
+
 
 cleanup:
 	//A3200 Cleanup
