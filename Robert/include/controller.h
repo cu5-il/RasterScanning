d3f33ded7a171c8bfc584944266d128689521bfb
Chris Urbanski <cu5@illinois.edu>
2022-05-04 13:11:18 -0500

    Combined the auger and velocity controller into a generic P type controller

--- Robert/include/controller.h
+++ Robert/include/controller.h
@@ -10,9 +10,6 @@ class Controller
 protected:
 	double _minE, _maxE, _minF, _maxF;
 
-	void setAugerLimits(double minE, double maxE) { _minE = minE; _maxE = maxE; }
-	void setFeedLimits(double minF, double maxF) { _minF = minF; _maxE = maxF; }
-
 public:
 	Controller() 
 		: _minE(-10), _maxE(10), _minF(0), _maxF(5) {}
@@ -20,71 +17,44 @@ public:
 		: _minE(minE), _maxE(maxE), _minF(minF), _maxF(maxF) {}
 
 	virtual void nextPath(Path& nextPath, Path prevPath, double errWd, double errCl) = 0;
+	void setAugerLimits(double minE, double maxE) { _minE = minE; _maxE = maxE; }
+	void setFeedLimits(double minF, double maxF) { _minF = minF; _maxE = maxF; }
+
 };
 
 // Auger controller class
-class AugerController : public Controller
+class PController : public Controller
 {
 public:
-	AugerController()
+	PController()
 		:_kp(1) {}
-	AugerController(MaterialModel augerModel)
-		: _model(augerModel), _kp(1) {}
-	AugerController(MaterialModel augerModel, double kp)
-		: _model(augerModel), _kp(kp) {}
-	AugerController(MaterialModel augerModel, double minE, double maxE)
-		: _model(augerModel), _kp(1) { Controller:setAugerLimits(minE, maxE);}
-	AugerController(MaterialModel augerModel,double kp, double minE, double maxE)
-		: _model(augerModel), _kp(kp) {Controller:setAugerLimits(minE, maxE);}
+	PController(MaterialModel model)
+		: _model(model), _kp(1) {}
+	PController(MaterialModel model, double kp)
+		: _model(model), _kp(kp) {}
 
 	void nextPath(Path& nextPath, Path prevPath, double errWd, double errCl)
 	{
 		double prevWidth = _model.width(prevPath.e, prevPath.f);
-		double nextE;
+		double nextInput;
 		if (!isnan(errWd)) {
-			nextE = _model.output(prevWidth + _kp* errWd, prevPath.f);
-			// check if output is saturated
-			if (nextE < _minE) { nextE = _minE; }
-			if (nextE > _maxE) { nextE = _maxE; }
-			nextPath.e = nextE;
-		}
-	}
-
-	void kp(double kp)
-	{
-		_kp = kp;
-	}
-
-private:
-	MaterialModel _model;
-	double _kp;
-};
-
-// Velocity controller class
-class VelocityController : public Controller
-{
-public:
-	VelocityController()
-		:_kp(1) {}
-	VelocityController(MaterialModel velocityModel)
-		: _model(velocityModel), _kp(1) {}
-	VelocityController(MaterialModel velocityModel, double kp)
-		: _model(velocityModel), _kp(kp) {}
-	VelocityController(MaterialModel velocityModel, double minF, double maxF)
-		: _model(velocityModel), _kp(1) { Controller:setFeedLimits(minF, maxF); }
-	VelocityController(MaterialModel velocityModel, double kp, double minF, double maxF)
-		: _model(velocityModel), _kp(kp) { Controller:setFeedLimits(minF, maxF); }
-
-	void nextPath(Path& nextPath, Path prevPath, double errWd, double errCl)
-	{
-		double prevWidth = _model.width(prevPath.e, prevPath.f);
-		double nextF;
-		if (!isnan(errWd)) {
-			nextF = _model.output(prevWidth + _kp * errWd, prevPath.f);
-			// check if output is saturated
-			if (nextF < _minF) { nextF = _minF; }
-			if (nextF > _maxF) { nextF = _maxF; }
-			nextPath.f = nextF;
+			switch (_model.type())
+			{
+			case MaterialModel::AUGER:
+				nextInput = _model.output(prevWidth + _kp * errWd, prevPath.f);
+				// check if output is saturated
+				if (nextInput < _minE) { nextInput = _minE; }
+				if (nextInput > _maxE) { nextInput = _maxE; }
+				nextPath.e = nextInput;
+				break;
+			case MaterialModel::VELOCITY:
+				nextInput = _model.output(prevWidth + _kp * errWd, prevPath.e);
+				// check if output is saturated
+				if (nextInput < _minF) { nextInput = _minF; }
+				if (nextInput > _maxF) { nextInput = _maxF; }
+				nextPath.f = nextInput;
+				break;
+			}
 		}
 	}
 
