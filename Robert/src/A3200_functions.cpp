3bbb58a7e5c7960e87d06bfbe5d9197b6a400ffd
Chris Urbanski <cu5@illinois.edu>
2022-05-07 19:00:19 -0500

    Changed output message

--- Robert/src/A3200_functions.cpp
+++ Robert/src/A3200_functions.cpp
@@ -5,6 +5,6 @@
 void A3200Error() {
 	CHAR data[1024];
 	A3200GetLastErrorString(data, 1024);
-	std::cout << "\a A3200 Error: " << data << std::endl;
+	std::cout << "\aA3200 Error: " << data << std::endl;
 	system("pause");
 }
