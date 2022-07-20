a7f09aa4ebf972a871774069680d3ab102c35db7
Chris Urbanski <cu5@illinois.edu>
2022-07-20 16:25:38 -0500

    Added more info when outputting the A3200 error

--- Robert/src/A3200_functions.cpp
+++ Robert/src/A3200_functions.cpp
@@ -1,10 +1,19 @@
 #include "A3200_functions.h"
 #include <iostream>
 #include "A3200.h"
+#include <thread>
+#include<source_location>
 
-void A3200Error() {
+void A3200Error(const std::source_location location) {
 	CHAR data[1024];
 	A3200GetLastErrorString(data, 1024);
-	std::cout << "\aA3200 Error: " << data << std::endl;
-	system("pause");
+	std::cout << "\a" 
+		<< "Thread(" << std::this_thread::get_id() << ") "
+		<< "File: " << location.file_name() << "("
+		<< location.line() << ":"
+		<< location.column() << ") `"
+		<< location.function_name() << "`: "
+		<< "A3200 Error: " << data << std::endl;
+	//system("pause");
+	Sleep(10);
 }
