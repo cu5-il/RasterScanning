0f4b0cacff36f4ef540ed47bee09dd8fc7f10387
Chris Urbanski <cu5@illinois.edu>
2022-07-21 18:21:02 -0500

    Added call location when A3200 error occurs

--- Robert/include/A3200_functions.h
+++ Robert/include/A3200_functions.h
@@ -1,9 +1,10 @@
 #pragma once
+#include<source_location>
 
 #ifndef A3200_FNS_H
 #define A3200_FNS_H
 
 /// @brief Prints the most recent error from the A3200.
-void A3200Error();
+void A3200Error(const std::source_location location = std::source_location::current());
 
 #endif // A3200_FNS_H
